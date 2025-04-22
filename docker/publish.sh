60da443be0bff18e32b7b6a592b3eed37d581880
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2025-04-22 21:37:55 +0400

    Update SDK version (#103)
    * Add requirements file to include supervisely socket-test dependency
    
    * Update docker image and instance version to 1.0.31 and 6.12.44 respectively; update supervisely dependency to 6.73.350
    
    * Remove supervisely socket-test dependency from requirements file

--- docker/publish.sh
+++ docker/publish.sh
@@ -1,2 +1,2 @@
-docker build -t supervisely/data-nodes:1.0.30 . && \
-docker push supervisely/data-nodes:1.0.30
+docker build -t supervisely/data-nodes:1.0.31 . && \
+docker push supervisely/data-nodes:1.0.31
